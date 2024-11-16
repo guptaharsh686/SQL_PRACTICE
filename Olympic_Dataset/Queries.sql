ee09cc5bccb9c72e43698854ad661e9a55cdfd34
Harsh Gupta <56353320+guptaharsh686@users.noreply.github.com>
2024-11-16 13:19:55 +0530

    Update Queries.sql

--- Olympic_Dataset/Queries.sql
+++ Olympic_Dataset/Queries.sql
@@ -42,5 +42,54 @@ Problem Statement: Write a SQL query to return the Olympic Games which had the h
     ,concat(first_value(games) over(order by no_of_countries desc),'-',first_value(no_of_countries) over(order by no_of_countries desc)) as most_countries
   from cte;
 
-5.
+5. Which nation has participated in all of the olympic games
+
+Problem Statement: SQL query to return the list of countries who have been part of every Olympics games.
+
+
+with cte2 as
+(
+	with cte as
+	(
+		select
+			country,count(*)
+		from
+		(select
+				games,nc.region as country
+				from olympics_history oh
+				inner join olympics_history_noc_regions nc on oh.noc = nc.noc
+				group by games,nc.region)
+		group by country
+	)
+	select 
+		*,
+		rank() over(order by count desc) as rnk
+	from cte
+)
+select
+	country,count
+from cte2
+where rnk = 1
+order by 1;
+
+
+
+
+      with tot_games as
+              (select count(distinct games) as total_games
+              from olympics_history),
+          countries as
+              (select games, nr.region as country
+              from olympics_history oh
+              join olympics_history_noc_regions nr ON nr.noc=oh.noc
+              group by games, nr.region),
+          countries_participated as
+              (select country, count(1) as total_participated_games
+              from countries
+              group by country)
+      select cp.*
+      from countries_participated cp
+      join tot_games tg on tg.total_games = cp.total_participated_games
+      order by 1;
+
     
